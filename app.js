00739be909db2405e71e4a51837ef4991bfc7f62
Jared Hensley <jahensle85@me.com>
2016-02-05 12:44:14 -0600

    initial push

---
+++ app.js
@@ -0,0 +1,11 @@
+// Initialize collapse button
+
+
+$(document).ready(function () {
+  $(".button-collapse").sideNav();
+  $('.materialboxed').materialbox();
+  $(".dropdown-button").dropdown({
+    hover: true
+  });
+  $('.parallax').parallax();
+});
