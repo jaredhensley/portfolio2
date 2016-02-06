124a6a222d9375cff4f4dcaf5b80301fa84ba284
Jared Hensley <jahensle85@me.com>
2016-02-06 16:23:40 -0600

    more hero work

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 $(document).ready(function () {
 
+  /*hamburger button*/
   $(".button-collapse").sideNav();
 
+  /*hmm not sure*/
   $('.materialboxed').materialbox();
 
+  /*enables dropdown on hover in navbar*/
   $(".dropdown-button").dropdown({
     hover: true
   });
 
+  /*loads parallax plugin*/
   $('.parallax').parallax();
+
+  /*on-click event handler for opening sayhello modal from hero image*/
+  $('.btn-resume').on('click', function () {
+    $('#modal1').openModal();
+  });
+
+  /*on-click event handler for submissions of sayhello modal*/
+  $('.modal-close').on('click', function () {
+    $('.sayhelloform')[0].reset();
+  });
+
 
 });
