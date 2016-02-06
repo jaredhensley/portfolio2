e01e5205341ad80a7c0a400f54b278d97dfdb59a
Jared Hensley <jahensle85@me.com>
2016-02-06 10:15:39 -0600

    hero img updates

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-// Initialize collapse button
-
-
 $(document).ready(function () {
+
   $(".button-collapse").sideNav();
+
   $('.materialboxed').materialbox();
+
   $(".dropdown-button").dropdown({
     hover: true
   });
+
   $('.parallax').parallax();
+
 });
